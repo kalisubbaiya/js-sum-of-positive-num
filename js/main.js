a0f230cd6b6d68fffbe19f3dab54f077e0a68a8c
kaliraj <kalisdvk96@gmail.com>
2022-12-14 10:11:20 +0530

    sum positive numbers only

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let a=parseInt(prompt("Enter the length"));
+let sum=0;
+for(i=1;i<=a;i++){
+    let b=parseInt(prompt("Enter a number"))
+    if(b>0){
+        sum+=b;
+    }
+}
+document.write(sum);
